90f7b87e03d84c538d8249c5a998f472584b585a
Nikita Kirilov <nkirilov7even@gmail.com>
2024-07-07 18:37:56 +0300

    add function to search tasks by name and function to get first ten tasks

---
+++ js/search.js
@@ -0,0 +1,19 @@
+import { getFirstTasks, displayTasks } from './tasks.js'
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  const search = document.getElementById('task-search');
+  const tasks = document.getElementById('tasks');
+
+  search.addEventListener('input', function (event) {
+    tasks.innerHTML = "";
+    const query = this.value
+    if (query === "") {
+      getFirstTasks();
+    }else {
+      fetch(`https://todo.doczilla.pro/api/todos/find?q=${query}`)
+        .then((response) => response.json())
+        .then((data) => displayTasks(data, data.length))
+    }
+  });
+});
---
+++ js/tasks.js
@@ -0,0 +1,48 @@
+function getTasks() {
+  document.addEventListener("DOMContentLoaded", (event) => {
+    getFirstTasks();
+  })
+}
+
+export function getFirstTasks() {
+  fetch("https://todo.doczilla.pro/api/todos?limit=10")
+    .then((response) => response.json())
+    .then(data => displayTasks(data, data.length > 10 ? 10 : data.length));
+}
+
+export function displayTasks(data, limit) {
+  const tasks = document.getElementById("tasks");
+
+  for (let i = 0; i < limit; i++) {
+    const task = document.createElement("div");
+    task.className = "task";
+    task.id = data[i].id;
+
+    const name = document.createElement("h2");
+    name.className = "task-name";
+    name.textContent = data[i].name;
+
+    const shortDesc = document.createElement("p");
+    shortDesc.className = "task-short-desc";
+    shortDesc.textContent = data[i].shortDesc;
+
+    const fullDesc = document.createElement("p");
+    fullDesc.className = "task-full-desc";
+    fullDesc.hidden = true;
+    fullDesc.textContent = data[i].fullDesc;
+
+    const status = document.createElement("input");
+    status.className = "task-status";
+    status.type = "checkbox";
+    status.checked = data[i].status;
+
+    const date = document.createElement("p");
+    date.className = "task-date;"
+    date.hidden = true;
+    date.textContent = data[i].date;
+
+    task.append(name, shortDesc, status, date, fullDesc)
+    tasks.appendChild(task);
+  }
+}
+getTasks();
